266ed9ea79baa31549796e35733407ca5b08da39
Leonardo <Leonardo.Yakub@gmail.com>
2022-06-04 14:48:19 +0200

    done

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
 import App from './App';
+import './style.css';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
